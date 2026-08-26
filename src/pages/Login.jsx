e86b9a11ea966b6a9723e5fc5c608a81451cffcc
inatbalthazar <inatbalthazar@gmail.com>
2026-08-26 15:59:52 +0800

    feat: implement login with mock data, add validation, and style UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,36 +1,48 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { users } from "../mock-data/index" //เดี๋ยวลอง mock user ก่อน
 
 function Login() {
     const [email, setEmail] = useState("");
     const [pwd, setPwd] = useState("");
+    const navigate = useNavigate(); //hook ไปหน้า admin ตามเงื่อนไข
 
+
+    // ลองใช้ user จาก users.js
     const handleLogin = (event) => {
         event.preventDefault();
-        // แกล้งๆเข้าสู่ระบบ
-        alert("เข้าสูระบบแล้ว");
+        const isUser = users.find(user => user.email === email);
+        if(isUser){
+          alert(`ยินดีต้อนรับ ${isUser.firstName} (${isUser.role}) จ้า`);
+        } else {
+          alert("เข้าสูระบบแล้ว");
+        }
+        navigate("/src/pages/Login.jsx"); /**** อย่าลืมเปลี่ยนฮุกไปหน้า admin ****/ 
     };
-
     return (
-    <div>
-      <h2>เข้าสู่ระบบ</h2>
-      <form onSubmit={handleLogin}>
+    <div className="max-w-md mx-auto my-10 p-6 bg-white rounded-lg shadow-md">
+      <h2 className="text-2xl font-bold text-center text-amber-900 mb-5">เข้าสู่ระบบ</h2>
+      <form onSubmit={handleLogin} className="space-y-4">
         <div>
-          <label>อีเมล</label>
+          <label className="block mb-1 font-medium">อีเมล</label>
           <input 
             type="email" 
-            required 
+            required
             value={email} 
-            onChange={event => setEmail(event.target.value)}  
+            onChange={event => setEmail(event.target.value)}
+            placeholder="  example@email.com"
+            className="w-full border p-2 rounded focus:ring-2 focus:ring-amber-500"
           />
         </div>
         <div>
-          <label>รหัสผ่าน</label>
+          <label className="block mb-1 font-medium">รหัสผ่าน</label>
           <input 
             type="password" 
             required 
             value={pwd} 
             onChange={event => setPwd(event.target.value)} 
+            placeholder="  **********"
+            className="w-full border p-2 rounded focus:ring-2 focus:ring-amber-500"
           />
         </div>
         <button type="submit">
